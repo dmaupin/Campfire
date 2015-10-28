7fa45de5353d16e14830409f3a86f8358870b96e
dmaupin <dm@davidmaupin.com>
2015-10-28 16:46:16 -0700

    adds working carrierwave upload

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,29 +1,42 @@
 class RecipesController < ApplicationController
 	def index
-		@recipe = Recipe.all
+		@recipes = Recipe.all
 	end
 
 	def show
-		
+		@recipe = Recipe.find(params[:id])
 	end
 
 	def edit
-		
+		@recipe = Recipe.find(params[:id])
 	end
 
 	def update
-		
+		@recipe = Recipe.find(params[:id])
+
+		if @recipe.update_attributes(params.require(:recipe).permit(:title, :description, :steps, :img))
+			redirect_to recipes_path
+		else
+			render "edit"
+		end		
 	end
 
 	def new
-		
+		@recipe = Recipe.new
 	end
 
 	def create
-		
+		@recipe = Recipe.new(params.require(:recipe).permit(:title, :description, :steps, :img))
+
+		if @recipe.save
+			redirect_to recipes_path
+		else
+			render "new"
+		end
 	end
 
 	def destroy
 		
 	end
+
 end
