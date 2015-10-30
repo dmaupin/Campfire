989c15578cdd653910c15e824c968f534976e1e6
dmaupin <dm@davidmaupin.com>
2015-10-29 20:46:18 -0700

    adds jquery function for scroll to top of recipes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,3 +12,13 @@ function add_fields(link, association, content) {
   var regexp = new RegExp("new_" + association, "g")
   $(link).parent().before(content.replace(regexp, new_id));
 }
+
+// scroll-to
+
+$(document).ready(function() {
+  $("#home").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#top").offset().top-38
+    }, 1000);
+  });
+});
